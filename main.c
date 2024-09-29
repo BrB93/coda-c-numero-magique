35a237c4b0e951e958ae675befac2c52307fdcf0
Brb93 <bryanbrik@gmail.com>
2024-09-29 07:04:47 +0200

    etape 2 : ajout des variables

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
+	int count = 0;
+	srand(time(NULL));
+	int random_number = rand() % 100 + 1;
 
+	int nombre_utilisateur;
+	printf("Veuillez rentrer un nombre entier : \n" );
+	scanf("%d", &nombre_utilisateur);
 
+	while (count < 10)
+	{
+		if (nombre_utilisateur < random_number)
+		{
+			printf("Votre nombre est trop petit! \n");
+			count = count + 1;
+		}
+		else if (nombre_utilisateur > random_number)
+		{
+			printf("Votre nombre est trop grand \n");
+			count = count + 1;
+		}
+		else if (nombre_utilisateur == random_number)
+		{
+			("Bravo! \n");
+			count = count + 1;
+			
 
+		}
+
+	}
 
 
 
