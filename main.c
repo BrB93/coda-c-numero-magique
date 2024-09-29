17a84eac9f32c93139ede86a2095b7afee8bf774
Brb93 <bryanbrik@gmail.com>
2024-09-29 08:43:21 +0200

    etape 6 : recherche erreur double appel scanf

--- main.c
+++ main.c
@@ -9,27 +9,38 @@ int main()
 	int random_number = rand() % 100 + 1;
 
 	int nombre_utilisateur;
-	printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
+	printf("Veuillez rentrer uniquement un nombre entier entre 1 et 100 : \n");
 	scanf("%d", &nombre_utilisateur);
+
+//Essais resolution du double appel scanf lors de la saisies du nombre entier.
+	
 
 
 	while (count < 10)
 	{
-		
+
+		if (scanf("%d", &nombre_utilisateur) != 1) 
+		{
+        	printf("Vous n'avez pas renseigner un nombre entier entre 1 et 100, le jeu se termine, veuillez le relancer !\n");
+        	return 1;
+        }
+
+		if (nombre_utilisateur < 1 || nombre_utilisateur > 100)
+		{
+			printf("Faites attention à bien renseigner un nombre entier entre 1 et 100 ! \n Allez-y :\n");
+			count ++;
+			continue;
+		}
+
+
 		if (nombre_utilisateur < random_number)
 		{
 			printf("Votre nombre est trop petit ! \n");
-			count = count + 1;
-			printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
-			scanf("%d", &nombre_utilisateur);
 		}
 		
 		else if (nombre_utilisateur > random_number)
 		{
 			printf("Votre nombre est trop grand ! \n");
-			count = count + 1;
-			printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
-			scanf("%d", &nombre_utilisateur);
 		}
 		
 		else if (nombre_utilisateur == random_number)
@@ -37,19 +48,12 @@ int main()
 			printf("Bravo ! \n");
 			break;
 		}
-
-		else if (nombre_utilisateur > 100, nombre_utilisateur != int)
-		{
-			printf("Faites attention à bien renseigner un nombre entier entre 1 et 100 ! Allez-y :\n");
-			count = count + 1;
-			scanf("%d", &nombre_utilisateur);
-		}
-
-
-	}
-	if (count = 10)
+	count ++;
+	if (count == 10)
 	{
-		printf("Vos 10 tentatives sont épuisées, Veuillez relancer le jeu. \n");
+		printf("Vos 10 tentatives sont épuisées, le nombre magique était %d. \n Veuillez relancer le jeu. \n" , random_number);
+	}
+
 	}
 
 	return 0;
