9ea66970c6b05330a4dc581e92eebd8fd180b0e2
Brb93 <bryanbrik@gmail.com>
2024-09-29 07:32:14 +0200

    etape 4 : stabilisation du parcours

--- main.c
+++ main.c
@@ -15,37 +15,30 @@ int main()
 
 	while (count < 10)
 	{
-		printf("Choisissez un nombre entier entre 1 et 100 :\n");
+		
 		if (nombre_utilisateur < random_number)
 		{
-			printf("Votre nombre est trop petit! \n");
+			printf("Votre nombre est trop petit ! \n");
 			count = count + 1;
 			printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
 			scanf("%d", &nombre_utilisateur);
-
 		}
+		
 		else if (nombre_utilisateur > random_number)
 		{
-			printf("Votre nombre est trop grand \n");
+			printf("Votre nombre est trop grand ! \n");
 			count = count + 1;
 			printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
 			scanf("%d", &nombre_utilisateur);
-			
-
 		}
+		
 		else if (nombre_utilisateur == random_number)
 		{
-			printf("Bravo! \n");
-			count = count + 1;
-			printf("Veuillez rentrer un nombre entier entre 1 et 100 : \n");
-			scanf("%d", &nombre_utilisateur);
-			
-
+			printf("Bravo ! \n");
+			count = 10;
 		}
 
 	}
-
-
 
 	return 0;
 }
